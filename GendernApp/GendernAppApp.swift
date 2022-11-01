4d4beccd9a3a3be292286a28075c91ce85aa0259
Yannik Ehrhardt <yannik-ehrhardt@gmx.de>
2022-11-01 11:28:05 +0100

    Initial Commit

---
+++ GendernApp/GendernAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GendernAppApp.swift
+//  GendernApp
+//
+//  Created by Yannik Ehrhardt on 01.11.22.
+//
+
+import SwiftUI
+
+@main
+struct GendernAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
