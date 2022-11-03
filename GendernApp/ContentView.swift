f3263b9ab2e86910cc23257ce5aa62a40ae2c8c5
Hannah Gürsching <h.guersching@gmx.net>
2022-11-03 16:43:37 +0100

    Noch mehr Änderungen und Experimente im ContentView

--- GendernApp/ContentView.swift
+++ GendernApp/ContentView.swift
@@ -9,27 +9,30 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
     var body: some View {
-        
-        NavigationView{
-            Text("Spielerisch Gendern lernen")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.purple)
-                .multilineTextAlignment(.center)
-                .navigationTitle("Gendern")
+        VStack {
+            
+            Image("Genderzeichen")
+                .imageScale(.small)
+                .cornerRadius(20)
+            Text("Gendern lernen")
+                    .font(.title)
+                    .fontWeight(.regular)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.833, green: 0.615, blue: 0.984)/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+            Button("Hier klicken") {
+            }
+            .accessibilityAddTraits([.isButton])
+            .frame(width: 200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0)
+            .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+            .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonColor")/*@END_MENU_TOKEN@*/)
+            
         }
-        Button("Hier klicken") {
-        }
-        .accessibilityAddTraits([.isButton])
-        .frame(width: 200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0)
-        .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-        
+        .padding()
     }
 }
 
-        
+    
         
         
         
