f35e0c7994d5c4587a0f7d49deb8e931aedea1b2
Hannah Gürsching <h.guersching@gmx.net>
2022-11-04 12:24:19 +0100

    ContentView: neue Views erstellt, die über die Buttons aufrufbar sind

--- GendernApp/ContentView.swift
+++ GendernApp/ContentView.swift
@@ -9,30 +9,58 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State private var secondView = false
+    @State private var thirdView = false
+    @State private var fourthView = false
+    @State private var fithtView = false
+    
     var body: some View {
-        VStack {
-            
-            Image("Genderzeichen")
-                .imageScale(.small)
-                .cornerRadius(20)
-            Text("Gendern lernen")
+        NavigationView {
+            VStack {
+                NavigationLink(destination:  VStack {
+                    Text ("Wähle eine Kategorie")
+                NavigationLink(destination: Text("ThirdView"), isActive: $thirdView) {EmptyView()}
+                NavigationLink(destination: Text("fourthView"), isActive: $fourthView) {EmptyView()}
+                NavigationLink(destination: Text("fithtView"), isActive: $fithtView) {EmptyView()}
+                    
+                List {
+                    Button ("Universität/Lehre"){
+                           thirdView = true
+                        }
+                    Button ("Schule"){
+                            fourthView = true
+                        }
+                    Button ("Alltag"){
+                        fithtView = true
+                        }
+                    }
+                }
+                               , isActive: $secondView) { EmptyView() }
+                
+                Image("Genderzeichen")
+                    .imageScale(.small)
+                    .cornerRadius(20)
+                Text("Gendern lernen")
                     .font(.title)
                     .fontWeight(.regular)
                     .foregroundColor(/*@START_MENU_TOKEN@*/Color(red: 0.833, green: 0.615, blue: 0.984)/*@END_MENU_TOKEN@*/)
                     .multilineTextAlignment(.center)
-            Button("Hier klicken") {
-                print("Hello")
-                //hier kann man coden, was der Button tun soll
+                Button("Hier klicken") {
+                    self.secondView = true
+                }
+                .accessibilityAddTraits([.isButton])
+                .frame(width: 200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0)
+                .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonColor")/*@END_MENU_TOKEN@*/)
+                
             }
-            .accessibilityAddTraits([.isButton])
-            .frame(width: 200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0)
-            .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
-            .foregroundColor(/*@START_MENU_TOKEN@*/Color("ButtonColor")/*@END_MENU_TOKEN@*/)
-            
+            .padding()
         }
-        .padding()
+        
     }
 }
+
 
     
         
