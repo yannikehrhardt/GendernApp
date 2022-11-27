122844eac8610ae532049dad756171e405357de4
Hannah Gürsching <h.guersching@gmx.net>
2022-11-27 15:53:51 +0100

    Änderungen Quizauswahl_Universitat

--- GendernApp/Views/Quizauswahl_Universitat.swift
+++ GendernApp/Views/Quizauswahl_Universitat.swift
@@ -10,13 +10,13 @@ import SwiftUI
 struct Quizauswahl_Universitat: View {
     var body: some View {
         VStack(spacing: 20){
+            
             Text("Pick a Quiz")
                 .font(.headline)
                 .fontWeight(.bold)
                 .foregroundColor(Color("TextColor"))
-                .padding(.bottom, 20.0)
+            //hier kommen die Quizze rein hin
             
-            // an dieser Stelle sollte jetzt die Funktion für das automatische generieren von Buttons eingefügt werden
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Image("Backgrounds App"))
