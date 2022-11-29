4ba8775c4efc0f28c54e6345197d871603c4939f
Yannik Ehrhardt <yannik-ehrhardt@gmx.de>
2022-11-29 15:20:18 +0100

    Loop hinzugefügt, um für jeden Quiz einen Button/Text zu erstellen

--- GendernApp/Questions.swift
+++ GendernApp/Questions.swift
@@ -8,3 +8,7 @@
 import Foundation
 import SwiftUI
 
+let quiz1 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "hallo1?", correctAnswer: "hallo", incorrectAnswers: ["wrong", "wrong"], answered: false)
+let quiz2 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "hallo2?", correctAnswer: "hallo", incorrectAnswers: ["wrong", "wrong"], answered: false)
+
+var quizze : [Quiz] = [quiz1, quiz2]
--- GendernApp/Views/Quizauswahl_Universitat.swift
+++ GendernApp/Views/Quizauswahl_Universitat.swift
@@ -8,16 +8,24 @@
 import SwiftUI
 
 struct Quizauswahl_Universitat: View {
+    
+    func creator(givenquiz : Quiz) -> any View {
+        print(givenquiz.question) as! (any View)
+    }
+    
     var body: some View {
         VStack(spacing: 20){
             
-            Text("Pick a Quiz")
-                .font(.headline)
-                .fontWeight(.bold)
-                .foregroundColor(Color("TextColor"))
-            //hier kommen die Quizze rein hin
-            
-            
+            ForEach(quizze, id: \.self.id) { quiz in
+                            Text(quiz.question)
+                                .foregroundColor(Color.white)
+                                .padding()
+                                .padding(.horizontal)
+                                .background(Color("ButtonColor"))
+                                .cornerRadius(20)
+                                .shadow(radius:10)
+                                
+                        }
             
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
