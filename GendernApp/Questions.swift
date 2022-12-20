f5743c4cb9973f3d7d06c78e840f3447bbe4281d
Hannah Gürsching <h.guersching@gmx.net>
2022-12-20 16:47:56 +0100

    Fragen hinzugefügt

--- GendernApp/Questions.swift
+++ GendernApp/Questions.swift
@@ -11,8 +11,11 @@ import Foundation
 import SwiftUI
 
 let quiz1 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "Wie wird 'Studenten' richtig gegendert?", correctAnswer: "Studierende", allAnswers: ["Studierende", "ist richtig so", "Studentinnen"], answered: true, furtherInformation: "hello")
-let quiz2 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "dummyQuestion?", correctAnswer: "richtig", allAnswers: ["richtig", "falsch", "wrong"], answered: true, furtherInformation: "hello2")
-let quiz3 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "Uni-Frage", correctAnswer: "ja", allAnswers: ["ja","nein", "wrong"], answered: false, furtherInformation: "")
+
+let quiz2 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "Wie können alle Geschlechtsidentitäten angesprochen werden sollen?", correctAnswer: "Student*innen", allAnswers: ["Student*innen", "StudentIinnen", "Studenten und Studentinnen"], answered: true, furtherInformation: "Der Stern wird in den letzten Jahren zunehmend verwendet, da das Geschlecht nicht mehr nur als binäres System verstanden wird. Diese Lösung steht allerding nicht im Einklang mit der amtlichen Orthographie.")
+
+let quiz3 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "Wie kann man Mitarbeiter richtig gendern, um alle Geschlechtsidentitäten anzusprechen", correctAnswer: "Mitarbieter_innen", allAnswers: ["Mitarbieter_innen","Mitarbeiter und Mitarbeiterinnen", "MitarbeiterInnen"], answered: false, furtherInformation: "Ähnlich wie das * hat der Unterstrich die Intention, alle existierenden Geschlechter anzusprechen")
+
 let quiz4 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "hallo4?", correctAnswer: "hallo", allAnswers: ["hallo", "wrong", "wrong"], answered: false, furtherInformation: "")
 let quiz5 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "hallo5?", correctAnswer: "hallo", allAnswers: ["hallo", "wrong", "wrong"], answered: false, furtherInformation: "")
 let quiz6 = Quiz(id: UUID.init(), type: "gap text" , topic: "Uni", question: "hallo6?", correctAnswer: "hallo", allAnswers: ["hallo", "wrong", "wrong"], answered: false, furtherInformation: "")
