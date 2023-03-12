8d277cb46854addb10ad01b99339c2d21d286bab
Hannah Gürsching <h.guersching@gmx.net>
2023-03-12 15:51:18 +0100

    Mehr Links hinzugefügt in InfoLinkView

--- GendernApp/Components/InfoLinkView.swift
+++ GendernApp/Components/InfoLinkView.swift
@@ -9,19 +9,74 @@ import SwiftUI
 
 struct InfoLinkView: View {
     var body: some View {
-        VStack{
-          
-            Link("geschicktgendern", destination: URL(string: "https://geschicktgendern.de/")!)
-                    .padding(.bottom, 10)
+        VStack(){
+            HStack{
+                Image(systemName: "arrowshape.forward")
+                    .foregroundColor(.black)
+                
+                Link("geschicktgendern", destination: URL(string: "https://geschicktgendern.de/")!)
+                    
+                Spacer()
+            }
+            .frame(width: 350, height: 50)
             
             
-           Link("Gendern polarisiert auch in der jungen Generation", destination: URL(string: "https://www.spiegel.de/kultur/gendern-als-stolperfalle-gendern-polarisiert-auch-in-der-jungen-generation-a-2608c97f-9aeb-4859-b173-0d1a7a91e3d4")!)
-                .padding(.bottom, 10)
+            HStack{
+                Image(systemName: "arrowshape.forward")
+                    .foregroundColor(.black)
+                
+                Link("Gendern polarisiert auch in der jungen Generation", destination: URL(string: "https://www.spiegel.de/kultur/gendern-als-stolperfalle-gendern-polarisiert-auch-in-der-jungen-generation-a-2608c97f-9aeb-4859-b173-0d1a7a91e3d4")!)
+                Spacer()
+                    
+            }
+            .frame(width: 350, height: 60)
             
-            Link("Kritik an Gender-Sprache", destination: URL(string: "https://www.spiegel.de/kultur/kritik-an-gender-sprache-abenteuerliche-duden-kreationen-a-846e042d-dfa9-4077-a16d-9adb2f258322")!)
-                .padding(.bottom, 10)
+            HStack{
+                
+                Image(systemName: "arrowshape.forward")
+                    .foregroundColor(.black)
+                
+                Link("Kritik an Gender-Sprache", destination: URL(string: "https://www.spiegel.de/kultur/kritik-an-gender-sprache-abenteuerliche-duden-kreationen-a-846e042d-dfa9-4077-a16d-9adb2f258322")!)
+                Spacer()
+                    
+            }
+            .frame(width: 350, height: 60)
+            
+            
+            HStack{
+                
+                Image(systemName: "arrowshape.forward")
+                    .foregroundColor(.black)
+                
+                Link("Aktuelles zum Thema Gendern bei Welt", destination: URL(string: "https://www.welt.de/themen/gendern/")!)
+                Spacer()
+                    
+            }
+            .frame(width: 350, height: 60)
+            
+            HStack{
+                
+                Image(systemName: "arrowshape.forward")
+                    .foregroundColor(.black)
+                
+                Link("Aktuelles zum Thema Gender bei FAZ", destination: URL(string: "https://www.faz.net/aktuell/gesellschaft/thema/gender")!)
+                Spacer()
+                    
+            }
+            .frame(width: 350, height: 60)
+            
+            HStack{
+                
+                Image(systemName: "arrowshape.forward")
+                    .foregroundColor(.black)
+                
+                Link("Aktuelles zum Thema Gender bei Spiegel", destination: URL(string: "https://www.spiegel.de/thema/gender/")!)
+                Spacer()
+                    
+            }
+            .frame(width: 350, height: 60)
         }
-        .frame(width: 350, height: 600)
+        .frame(width: 350, height: 500)
         .background(Color("Color 1"))
         .cornerRadius(10)
     }
