eab64e947ad91b84444df2541722968934cf1d51
Hannah Gürsching <h.guersching@gmx.net>
2023-03-13 19:59:30 +0100

    Fragenchecker weiß und größer gemacht

--- GendernApp/Components/QuizButton.swift
+++ GendernApp/Components/QuizButton.swift
@@ -20,7 +20,7 @@ struct QuizButton: View {
         ScrollView{
             VStack(spacing: 15){
                 ForEach(givenQuiz, id: \.self.id) { quiz in   // foreach-Schleife zum durchsuchen des Array givenQuiz, der mit Quizzen beladen ist. Für jedes Quiz in givenquiz wird ein Button erstellt, den die question des Quizzes als Text übergeben bekommt
-                    VStack(spacing:20){
+                    VStack(spacing: 20){
                         NavigationLink(destination: QuizView(givenQuiz: quiz)){ //NavigationLink der beim Klicken des Buttons zur QuizView führt
                             Text(quiz.question)
                                 .font(.title3)
@@ -31,7 +31,10 @@ struct QuizButton: View {
                             
                             //wenn die Frage vom Spielenden bereits korrekt beantwortet wurde (dann ist die ID des Quizzes teil des Arrays aswered (in Players)), dann wird ein Haken hinter der Frage gesetzt
                             if(players.players[players.usernameOffset(players.currentplayer.username)].answered.contains(quiz.id)){
+                                
                                 Image(systemName: "checkmark.circle.fill")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 30))
                             }
                             
                         }
